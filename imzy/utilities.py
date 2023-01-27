84e19518ba6d0e05f45e6d5130782d8a75106c1a
Lukasz Migas <lukas.migas@yahoo.com>
2023-01-27 18:20:06 +0100

    Improved index searching

--- imzy/utilities.py
+++ imzy/utilities.py
@@ -45,9 +45,20 @@ def find_nearest_value(data: np.ndarray, value: ty.Union[int, float, np.ndarray,
     return data[idx]
 
 
-def find_nearest_index_sorted(array, value):
-    """Much quicker implementation of `find_nearest_index` if the data is sorted"""
-    return np.searchsorted(array, value, side="left")
+def find_nearest_index_batch(array: np.ndarray, values: np.ndarray):
+    """Find nearest index of asked value"""
+    # make sure array is a numpy array
+    array = np.array(array)
+    # get insert positions
+
+    idxs = np.searchsorted(array, values, side="left")
+
+    # find indexes where previous index is closer
+    prev_idx_is_less = (idxs == len(array)) | (
+        np.fabs(values - array[np.maximum(idxs - 1, 0)]) < np.fabs(values - array[np.minimum(idxs, len(array) - 1)])
+    )
+    idxs[prev_idx_is_less] -= 1
+    return idxs
 
 
 @numba.njit()
