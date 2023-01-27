9944754360848f97ea78f764446d98be6899eba2
Lukasz Migas <lukas.migas@yahoo.com>
2023-01-27 18:58:32 +0100

    Update __init__.py

--- imzy/_centroids/__init__.py
+++ imzy/_centroids/__init__.py
@@ -1,6 +1,4 @@
 """Centroids."""
 from ._hdf5_store import H5CentroidsStore  # noqa F401
-
-from ._zarr_store import ZarrCentroidsStore  # noqa F401
-
 from ._memory_store import InMemoryStore  # noqa F401
+from ._zarr_store import ZarrCentroidsStore  # noqa F401
