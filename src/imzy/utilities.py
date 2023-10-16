bfdfc5f9cf70b8ae0dd5ad3e24805d365ed78595
lukasz-migas <lukas.migas@yahoo.com>
2023-10-16 10:17:40 +0100

    Update utilities.py

--- src/imzy/utilities.py
+++ src/imzy/utilities.py
@@ -1,10 +1,46 @@
 """Utility functions."""
 import typing as ty
+from pathlib import Path
 
 import numba
 import numpy as np
 from koyo.spectrum import find_between_batch
+from koyo.typing import PathLike
 from scipy.interpolate import interp1d
+
+
+def get_rois(path: PathLike) -> ty.List[int]:
+    """Get ROIs from file."""
+    path = Path(path)
+    if path.suffix == ".imzML":
+        return [0]
+    elif path.suffix == ".d":
+        return get_rois_from_bruker_d(path)
+    else:
+        raise ValueError(f"Unsupported file type: {path.suffix}")
+
+
+def get_rois_from_bruker_d(path: PathLike) -> ty.List[int]:
+    """Get ROIs from Bruker .d file."""
+    import sqlite3
+
+    path = Path(path)
+    if path.suffix == ".d":
+        if (path / "analysis.tdf").exists():
+            path = path / "analysis.tdf"
+        else:
+            path = path / "analysis.tsf"
+    if not path.exists():
+        raise FileNotFoundError(f"File not found: {path}")
+
+    # make sqlite connection
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    cursor.execute("SELECT RegionNumber FROM MaldiFrameInfo ORDER BY RegionNumber DESC LIMIT 1")
+    last_roi = cursor.fetchone()[0]
+    cursor.close()
+    conn.close()
+    return list(range(0, last_roi + 1))
 
 
 def resample_ppm(new_mz: np.ndarray, mz_array: np.ndarray, intensity_array: np.ndarray):
