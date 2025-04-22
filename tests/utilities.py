633a69b0aa86dae67e2b145c205abf87b424dcb0
Lukasz Migas <lukas.migas@yahoo.com>
2025-04-22 20:35:06 +0100

    Migrate to using yoki5

--- tests/utilities.py
+++ tests/utilities.py
@@ -1,19 +1,19 @@
 from pathlib import Path
 
 
-def get_imzml_data():
+def get_imzml_data(pattern: str = "*.imzML") -> list[Path]:
     """Get data from the `_test_data` folder."""
     test_dir = Path(__file__).parent / "_test_data"
-    return list(test_dir.glob("*.imzML"))
+    return list(test_dir.glob(pattern))
 
 
-def get_tsf_data():
+def get_tsf_data(pattern: str = "*_tsf.d") -> list[Path]:
     """Get data from the `_test_data` folder."""
     test_dir = Path(__file__).parent / "_test_data"
-    return list(test_dir.glob("*_tsf.d"))
+    return list(test_dir.glob(pattern))
 
 
-def get_tdf_data():
+def get_tdf_data(pattern: str = "*_tdf.d"):
     """Get data from the `_test_data` folder."""
     test_dir = Path(__file__).parent / "_test_data"
-    return list(test_dir.glob("*_tdf.d"))
+    return list(test_dir.glob(pattern))
